e28e6d672c135133af93a86bedbb7190afc78274
sureshmoni4u <sureshmoni4u@gmail.com>
2019-12-10 07:03:24 +0530

    code added

---
+++ app/src/main/java/com/kathreshtech/mobtally/LandingActivity.java
@@ -0,0 +1,15 @@
+package com.kathreshtech.mobtally;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_landing_page);
+    }
+}
--- app/src/main/java/com/kathreshtech/mobtally/LoginActivity.java
+++ app/src/main/java/com/kathreshtech/mobtally/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.kathreshtech.mobtally;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.ac_login);
         btnSkip = findViewById(R.id.btn_skip);
         btnlogin = findViewById(R.id.btn_login);
+
+        btnSkip.setOnClickListener(this);
+        btnlogin.setOnClickListener(this);
     }
 
     @Override
@@ -23,12 +27,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         switch (v.getId()){
             case R.id.btn_skip :
-                
+                startLandingPageActivity();
                 break;
 
             case  R.id.btn_login :
+                startLandingPageActivity();
                 break;
         }
 
     }
+
+    private void startLandingPageActivity(){
+        Intent intent = new Intent(this,LandingActivity.class);
+        startActivity(intent);
+    }
+
 }
