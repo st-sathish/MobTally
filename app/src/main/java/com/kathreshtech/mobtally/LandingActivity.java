074925d702ffd471a44baa37fae2a52cbe473465
sureshmoni4u <sureshmoni4u@gmail.com>
2019-12-12 07:11:25 +0530

    navigation view

--- app/src/main/java/com/kathreshtech/mobtally/LandingActivity.java
+++ app/src/main/java/com/kathreshtech/mobtally/LandingActivity.java
@@ -1,9 +1,13 @@
 package com.kathreshtech.mobtally;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -11,5 +15,38 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_landing_page);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        setupToggle(drawerLayout,toolbar);
+
+    }
+
+    public void setupToggle(DrawerLayout drawerLayout, final Toolbar toolbar){
+        final ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+            R.string.open,R.string.close) {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+
+                toolbar.setAlpha(1-slideOffset/2);
+
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        drawerLayout.post(actionBarDrawerToggle::syncState);
     }
 }
